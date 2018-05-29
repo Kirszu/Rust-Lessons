94c189a38813a55c3f47754e198e6e58a5bfcfb1
Marcin Kirsz <marcinkirsz@wp.pl>
2018-05-29 18:19:28 +0200

    Second version

--- lesson8-fibonacci/src/main.rs
+++ lesson8-fibonacci/src/main.rs
@@ -6,15 +6,20 @@ fn main() {
     io::stdin().read_line(&mut input)
     .expect("Failed to read line");
     let mut num = input.trim().parse().expect("Not a number!");
+    let mut result = vec![1, 1];
 
-    fibonacci(num);
+    match num {
+        1 => println!("{}", 1),
+        2 => println!("{:?}", result),
+        _ => fibonacci(result, num),
+    };
 
-    fn fibonacci(num: u32) -> u32 {
-        println!("{}", num);
-        if num == 0 || num == 1  {
-            return num;
-        } else {
-            return fibonacci(num - 1) + fibonacci(num - 2);
+    fn fibonacci(mut vec: Vec<u32>, num: u32) -> () {
+        for x in 0..num - 4{
+            let mut item = vec[x];
+            let mut item2 = vec[x+1];
+            vec.push(item + item2);
         }
-    }    
+        println!("{:?}", vec);
+    }
 }
